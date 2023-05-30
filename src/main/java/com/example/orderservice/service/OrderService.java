acfaf144338291ddd39219529648bba44ae96ea8
Sergey Shamiryan <sergshamiryan@gmail.com>
2023-05-30 14:23:20 +0400

    WebClient

--- src/main/java/com/example/orderservice/service/OrderService.java
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -5,9 +5,9 @@ import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.model.OrderLineItems;
 import com.example.orderservice.model.Orders;
 import com.example.orderservice.repository.OrderRepository;
-import jakarta.persistence.criteria.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +17,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         List<OrderLineItems> orderLineItems = orderRequest.orderLineItemsList().stream()
@@ -27,7 +28,19 @@ public class OrderService {
                 .orderNumber(UUID.randomUUID().toString())
                 .orderLineItemsList(orderLineItems).build();
 
-        orderRepository.save(orders);
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if (result != null && result) {
+            orderRepository.save(orders);
+        } else {
+            throw new IllegalArgumentException("Not in stock");
+        }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsRequest orderLineItemsRequest) {
